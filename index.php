55ecc44f7770abee049321e36f3ca170ba3f20a1
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2020-07-15 23:26:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post" action="mail-process.php">
+Name:<br>
+<input type="text" name="name">
+<br>
+Email:<br>
+<input type="text" name="email">
+<br>
+Phone No.:<br>
+<input type="text" name="phone_no">
+<br>
+Message:<br>
+<input type="email" name="message">
+<br><br>
+<input type="submit" name="save" value="submit">
+</form>
+</body>
+</html>
---
+++ mail-process.php
@@ -0,0 +1,47 @@
+<?php
+include_once 'database.php';
+$first_name = $_POST['name'];
+$last_name = $_POST['email'];
+$city_name = $_POST['phone_no'];
+$email = $_POST['message'];
+
+/* sql query for inserting data into database */
+
+mysqli_query($conn,"insert into employee (name,email,phone_no,message) values ('$name','$email','$phone_no','$message')") or die(mysqli_error());
+require_once 'mailer/class.phpmailer.php';
+/* creates object */
+$mail = new PHPMailer(true);
+$mailid = $email;
+$subject = "Thank you";
+$text_message = "Hello";
+$message = "Thank You for Contact with us.";
+
+try
+{
+$mail->IsSMTP();
+$mail->isHTML(true);
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "ssl";
+$mail->Host = "smtp.gmail.com";
+$mail->Port = '465';
+$mail->AddAddress($mailid);
+$mail->Username ="fabcutsofficial@gmail.com";
+$mail->Password ="password@123";
+$mail->SetFrom('fabcutsofficial@gmail.com','Team FabCuts');
+$mail->AddReplyTo("fabcutsofficial@gmail.com","Team FabCuts");
+$mail->Subject = $subject;
+$mail->Body = $message;
+$mail->AltBody = $message;
+if($mail->Send())
+{
+echo "Thank you for register u got a notification through the mail you provide";
+}
+}
+catch(phpmailerException $ex)
+{
+$msg = "
+".$ex->errorMessage()."
+";
+}
+?>
